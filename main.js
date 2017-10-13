8d7903e6e6453780345b6d3e080fea33b3efe470
andreGarvin <andregarvin718@gmail.com>
2017-10-13 00:28:29 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+class Spark {
+
+    constructor() {
+        this.events = {};
+    }
+
+    emit(event, payload) {
+        payload = Array.from(arguments).slice(1)
+        if (this.events[event]) {
+            this.events[event](...payload)
+            
+            if (this.events['*'].events.includes(event)) {
+                this.events['*'].action(...payload)
+            }
+        }
+    }
+
+    on(event, action) {
+        if (event && action) {
+            if (typeof action === 'function' || typeof action === 'object') {
+                this.events[event] = action;
+            }
+        }
+    }
+
+    onAll(events, action) {
+        if (Array.isArray(events)) {
+            this.on('*', {
+                events,
+                action
+            })
+        } else if (events === '*') {
+            this.on('*', action)
+        }
+    }
+}
+
+const spark = new Spark()
+
+spark.on('hello', (payload) => {
+    console.log(payload)
+})
+
+spark.onAll(['hello'], () => {
+    console.log('Something was triggered');
+})
+
+spark.emit('hello', {
+    msg: "Hellloooooo"
+})
